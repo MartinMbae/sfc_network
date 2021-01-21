b877d4de353851fc1754d7ee19724b2eb70ce76d
Martin Mbae <martinmbae.codemaster@gmail.com>
2021-01-21 16:05:49 +0300

    Added user profile

--- lib/fragments/first_fragment.dart
+++ lib/fragments/first_fragment.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_network/pages/login.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class FirstFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+
+    Map<String, double> dataMap = {
+      "Resolved": 5,
+      "Unresolved": 3,
+    };
+
     return Center(
-      child: Text("Welcome to SFCFIBER"
+      child: PieChart(
+        dataMap: dataMap,
+        animationDuration: Duration(milliseconds: 1000),
+        chartLegendSpacing: 32,
+        chartRadius: MediaQuery.of(context).size.width / 2,
+        initialAngleInDegree: 0,
+        chartType: ChartType.disc,
+        legendOptions: LegendOptions(
+          showLegendsInRow: true,
+          legendPosition: LegendPosition.bottom,
+          showLegends: true,
+          legendShape: BoxShape.rectangle,
+          legendTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        chartValuesOptions: ChartValuesOptions(
+          showChartValueBackground: true,
+          showChartValues: true,
+          showChartValuesInPercentage: false,
+          showChartValuesOutside: false,
+        ),
       ),
     );
   }
