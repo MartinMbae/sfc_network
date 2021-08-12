99c6ffdcf3e49f54e9e569bf7a410307ef329d4d
martin <martinmbae.codemaster@gmail.com>
2021-08-12 07:35:14 +0300

    Added pagination to Manholes

--- lib/fragments/manhole_fragment.dart
+++ lib/fragments/manhole_fragment.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_awesome_alert_box/flutter_awesome_alert_box.dart';
 import 'package:flutter_network/manhole_holder.dart';
 import 'package:flutter_network/models/manhole.dart';
-import 'package:flutter_network/pages/empty_screen.dart';
 import 'package:flutter_network/utils/constants.dart';
 import 'package:flutter_network/utils/shared_pref.dart';
 import 'package:http/http.dart' as http;
@@ -15,67 +14,114 @@ class ManholePage extends StatefulWidget {
 }
 
 class _ManholePageState extends State<ManholePage> {
+
+  List<dynamic> initialManholes;
+  var scrollController = ScrollController();
+  bool updating = false;
+
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: fetchManholes(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<dynamic> manhole = snapshot.data;
-          bool hasData = manhole.length > 0;
-          return ListView.builder(
-              itemCount: manhole.length,
-              itemBuilder: (context, index) {
-                return hasData
-                    ? Column(
-                        children: [
-                          ManholeHolder(
-                            manhole: Manhole.fromJson(manhole[index]),
-                            num: index + 1,
-                          ),
-                          Divider(
-                            color: Colors.black,
-                          )
-                        ],
-                      )
-                    : EmptyPage(
-                        icon: Icons.error, message: "No manhole found");
-              });
-        } else {
-          return Center(child: CircularProgressIndicator());
+  void initState() {
+    super.initState();
+    getManholes();
+  }
+
+  getManholes() async {
+    initialManholes = await NewsApiHelper().fetchNews(context, "0");
+    setState(() {});
+  }
+
+  checkUpdate() async {
+    setState(() {
+      updating = true;
+    });
+    var scrollPosition = scrollController.position;
+    if (scrollPosition.pixels == scrollPosition.maxScrollExtent) {
+      List<dynamic> newIcsNews = await  NewsApiHelper().fetchNews(context, "${initialManholes.length}");
+      initialManholes.addAll(newIcsNews);
+    }
+
+    setState(() {
+      updating = false;
+    });
+  }
+
+  getManholeBody() {
+    if (initialManholes == null) return Center(child: CircularProgressIndicator());
+    return NotificationListener<ScrollNotification>(
+      onNotification: (noti) {
+        if (noti is ScrollEndNotification) {
+          checkUpdate();
         }
+        return true;
       },
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: initialManholes.length,
+                shrinkWrap: true,
+                controller: scrollController,
+                  itemBuilder: (context, index) {
+                    return  ManholeHolder(
+                          manhole: Manhole.fromJson(initialManholes[index]),
+                          num: index + 1,
+                        );
+                  },
+              ),
+            ),
+            if (updating) CircularProgressIndicator()
+          ],
+        ),
+      ),
     );
   }
 
-  Future<List<dynamic>> fetchManholes() async {
-    await Future.delayed(Duration(seconds: 2));
+
+  @override
+  Widget build(BuildContext context) {
+    return getManholeBody();
+  }
+
+}
+
+
+class NewsApiHelper {
+
+  Future<List<dynamic>> fetchNews( BuildContext context, String start) async {
+
+    var url = "${BASE_URL}index.php/v1/api/manhole";
     SessionManager prefs = SessionManager();
     var userId = await prefs.getId();
-    var url = "${BASE_URL}index.php/v1/api/manhole";
     Map<String, String> queryParams = {
       'id': "$userId",
+      'start':start,
+      'limit':"20"
     };
     String queryString = Uri(queryParameters: queryParams).query;
 
-    var requestUrl = url + '?' + queryString;
+    var requestUrl = url  + '?' + queryString;
     var response = await http.get(requestUrl).timeout(Duration(seconds: 30),
         onTimeout: () {
-      DangerAlertBox(
-          context: context,
-          messageText:
+          DangerAlertBox(
+              context: context,
+              messageText:
               "Action took so long. Please check your internet connection and try again.",
-          title: "Error");
-      return null;
-    });
+              title: "Error");
+          return null;
+        });
 
 
     if (response.statusCode != 200) {
       throw new Exception('Error fetching manholes');
     }
 
-    List<dynamic> manhole = json.decode(response.body);
+    List<dynamic> manholes = json.decode(response.body);
 
-    return manhole;
+    return manholes;
   }
+
+
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -10,33 +10,32 @@ void main() {
   runApp(MyApp());
 }
 
-
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext context){
+  HttpClient createHttpClient(SecurityContext context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+
     return ScreenUtilInit(
-      designSize: Size(360,690),
-      allowFontScaling: false,
-      child:  MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Network Manager',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          // visualDensity: VisualDensity.adaptivePlatformDensity,
+        designSize: Size(360, 690),
+        builder: () => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Network Manager',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            // visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: SplashScreen(),
         ),
-        home:  SplashScreen(),
-      ),
     );
   }
 }
